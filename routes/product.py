c3a85a72fd1d871139d7ca3727e39c464e5d0e3b
JeanSousa <jeanjr.silvasousa@gmail.com>
2025-04-09 19:57:19 -0300

    add some routes and openapi

--- routes/product.py
+++ routes/product.py
@@ -1,5 +1,21 @@
+from flask import redirect
+
 from . import blueprint 
 
-@blueprint.route('hello-world', methods=['GET'])
-def hello_world():
-    return 'ola mundo'
+@blueprint.route('/', methods=['GET'])
+def home():
+    return redirect('/openapi')
+
+
+
+@blueprint.route('products/<int:product_id>', methods=['GET'])
+def get(product_id):
+    return f"id do produto {product_id}"
+
+@blueprint.route('products', methods=['GET'])
+def get_all():
+    return { 'product': []}, 200
+
+@blueprint.route('products', methods=['POST'])
+def create():
+    return { 'product': []}, 201
--- server/instance.py
+++ server/instance.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask_openapi3 import OpenAPI, Info
 
 from routes import blueprint
 
 class Server():
     def __init__(self):
-        self.__app = Flask(__name__)
+        self.__info = Info(title="Minha API", version="1.0.0")
+        self.__app = OpenAPI(__name__, info=self.__info)
         self.__app.register_blueprint(blueprint) 
 
     def get_app(self):
