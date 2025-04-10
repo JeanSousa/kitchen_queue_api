ba31920fc8e27f8e2a9edb471a0555be8713a281
JeanSousa <jeanjr.silvasousa@gmail.com>
2025-04-10 14:04:54 -0300

    add error exceptions

--- models/product.py
+++ models/product.py
@@ -5,9 +5,21 @@ from models.base import BaseModel
 class Product(BaseModel):
     __tablename__ = 'product'
 
+    """Table definition."""
     id = Column("pk_product", Integer, primary_key=True)
     name = Column(String(200), unique=True)
     value = Column(Float)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     deleted_at = Column(DateTime, nullable=True)
+
+    def __init__(self, name, value):
+        """
+        Cria um Produto
+
+        Arguments:
+            name: nome do produto.
+            value: valor esperado para o produto
+        """
+        self.name = name 
+        self.value = value 
--- routes/product.py
+++ routes/product.py
@@ -1,17 +1,33 @@
-from pydantic import BaseModel, Field
+from sqlalchemy.exc import IntegrityError
 
-from server import product_tag
 from schemas import ProductSchema, ProductSchemaResponse, ProdutoPathSchema
 from . import api_blueprint 
+from models import Session, Product
+from server import product_tag
 
 
-@api_blueprint.post('/produtos', tags=[product_tag], responses={"201": ProductSchemaResponse})
+@api_blueprint.post('/products', tags=[product_tag], responses={"201": ProductSchemaResponse})
 def add(form: ProductSchema):
     """Adiciona um novo Produto à base de dados
 
     Retorna uma representação de um produto.
     """
-    return { "name": "Coca cola", "value": 2.65}, 201
+    product = Product(name=form.name, value=form.value)
+    
+    try:
+        session = Session()
+        session.add(product)
+        session.commit()
+
+        return { "name": "Coca cola", "value": 2.65}, 201
+    except IntegrityError as e:
+        # case product already exists
+        errorMessage = "Produto já existe na base de dados"
+        return { "message": errorMessage }, 409
+    except Exception as e:
+        # unknow error
+        errorMessage = "Não foi possivel salvar o item, por favor tente novamente mais tarde"
+        return { "message": errorMessage}, 400 
 
 @api_blueprint.get('/products/<int:product_id>', tags=[product_tag], responses={"200": ProductSchemaResponse})
 def get(path: ProdutoPathSchema):
